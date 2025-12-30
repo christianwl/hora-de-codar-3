47eaf245f4e5a117c12ae65c44ad3532ff6226bf
christianwl <chris2w0social@gmail.com>
2025-12-30 15:15:09 -0300

    feat: adicionar novas tags meta e functions.js para otimização de código

--- js/desafios.js
+++ js/desafios.js
@@ -1,4 +1,11 @@
+import { receberValorNumerico, receberValorPositivo } from "./functions.js";
+
 // 1 - Escreva um algoritmo para ler 2 valores informados pelo usuário e se o segundo valor informado for igual ou menor que ZERO, deve ser lido um novo valor. Ou seja, para o segundo valor não pode ser aceito o valor zero, nem um valor negativo.  O seu programa deve imprimir o resultado da divisão do primeiro valor lido pelo segundo valor. 
+
+const valoresDivisao = {
+    valorQualquer: receberValorNumerico("Digite o primeiro número "),
+    divisor: receberValorPositivo("Digite o segundo número: \n\nOBS: Esse número deve ser maior que 1!", {textoErroOpcional: "O número digitado não é maior que 1, tente novamente..."})
+}
 
 var primeiroValor = Number(prompt("Digite um valor: "));
 
