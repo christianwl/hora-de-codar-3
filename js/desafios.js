8cfe3a9e1de978b9da69c14ad672d8eacc12118b
christianwl <chris2w0social@gmail.com>
2025-12-31 17:50:43 -0300

    refactor: remover linha de codigo desnecessária

--- js/desafios.js
+++ js/desafios.js
@@ -15,8 +15,6 @@ var resultado = retornarDivisaoSimples(valoresDivisao);
 
 ApresentarNaTela(`O resultado da divisão de ${valoresDivisao.dividendo} pelo ${valoresDivisao.divisor} é ${resultado}`)
 
-var primeiroValor = Number(prompt("Digite um valor: "));
-
 // 2 - Crie uma bomba relógio (usando somente código - para deixar claro!) cuja contagem regressiva vá de 30 a 0. Utilize "document.write" para escrever em tela e no final da repetição escreva "EXPLOSÃO". 
 
 var indice = 30;
