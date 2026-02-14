27d9f519fbe7ee6dfbaf4962f3c2a4a354e7a6e6
christianwl <chris2w0social@gmail.com>
2026-02-14 05:34:03 -0300

    refactor: modularize code and improve structure

---
+++ src/scripts/app.js
@@ -0,0 +1,25 @@
+import {
+  startChallenge1,
+  startChallenge2,
+  startChallenge3,
+  startChallenge4,
+  startChallenge5,
+  startChallenge6,
+  startChallenge7,
+  startChallenge8,
+  startChallenge9,
+  startChallenge10,
+  startChallenge11,
+} from "./services/desafios.js";
+
+startChallenge1();
+startChallenge2();
+startChallenge3();
+startChallenge4();
+startChallenge5();
+startChallenge6();
+startChallenge7();
+startChallenge8();
+startChallenge9();
+startChallenge10();
+startChallenge11();
